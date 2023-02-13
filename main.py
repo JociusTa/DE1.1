2c40be34201f99523c6de3a859bf695c8183cf6d
JociusTa <tautvydasjocius@live.com>
2023-02-13 15:03:41 +0200

    Added docstrings

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ dice_results = []
 
 
 class DiceRoll:
+    """
+    A class for rolling dice
+    """
 
     def __init__(self, throws=3, count=5, sides=6):
+        """
+        Initialize the DiceRoll class with default values of 3 throws, 5 dice, and 6 sides.
+        """
         self.throws = throws
         self.count = count
         self.sides = sides
 
     def dice_roll(self):
+        """
+        Roll the dice the specified number of times, storing the results in the dice_results list.
+        """
         self.dice_ask_input()
         for throw in range(self.throws):
             one_dice_result = []
@@ -22,31 +31,44 @@ class DiceRoll:
             self.dice_print_list()
 
     def dice_truncate_list(self):
+        """
+        Truncate the dice_results list so that it never contains more than 100 elements.
+        """
         dice_results.append(dice_results[-1])
         if len(dice_results) > 100:
             dice_results.pop(0)
 
     def dice_print_list(self):
-        print("Sum of all values in dice_results:", sum(dice_results[-1]),"Full list of throws:",dice_results[-1])
+        """
+        Print the sum of all values in dice_results and the full list of throws.
+        """
+        print("Sum of all values in dice_results:", sum(dice_results[-1]), "Full list of throws:", dice_results[-1])
 
     def dice_interface(self):
+        """
+        Interact with the user to allow them to roll the dice, view the list of results, or exit the program.
+        """
         while True:
             user_input = input("Choose command(roll, print_list, exit): ")
             if user_input.lower() == "roll":
                 self.dice_ask_input()
                 self.dice_roll()
             elif user_input.lower() == "print_list":
-                self.print_dice_results()
+                print(dice_results)
             elif user_input.lower() == "exit":
                 break
             else:
                 print("Invalid command.")
 
     def dice_ask_input(self):
+        """
+        Ask the user for the number of dice, sides, and throws, or use the default values if no input is provided.
+        """
         while True:
             try:
                 user_input = input(
-                    'Enter integers for ammount of dice, sides, ammount of throws, separated by space, or enter for default(5 3 6)').strip()
+                    'Enter integers for amount of dice, sides, amount of throws, separated by space,'
+                    ' or enter for default(5 3 6)').strip()
                 if not user_input:
                     count, sides, throws = self.count, self.sides, self.throws
                     break
